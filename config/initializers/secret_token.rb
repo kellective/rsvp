32c48ed9a98836dfe162f48758723f36281a6416
kellective <kellective@gmail.com>
2013-06-15 15:14:29 +1200

    bare bones

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rsvp::Application.config.secret_token = 'b808167ea63fd322b9be12c12181ac5f248381235a7a871a9e0e9972da58210b983956fe083303411ec4f868a5504f2e296ce78b1f8e5f60e40ab9b5c3a3db78'
